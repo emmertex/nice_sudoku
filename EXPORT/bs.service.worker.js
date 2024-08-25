07bb331e886521708a3f182cb911c1ce8511b887
Andrew Frahn <emmertex@gmail.com>
2024-08-25 22:57:19 +1000

    new builds

--- EXPORT/bs.service.worker.js
+++ EXPORT/bs.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1724503649|131242166';
+const CACHE_VERSION = '1724590627|35726365066';
 /** @type {string} */
 const CACHE_PREFIX = 'Nice Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
